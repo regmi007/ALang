716f597a878f23580c851f65a4c7c5ca884f4fcb
Aayush Poudel <aayushpoudel@gmail.com>
2014-02-13 10:18:34 -0500

    -Read script from file. -CMake Modified.

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,12 +1,32 @@
 #include <stack>
+#include <stdio.h>
 
+extern FILE *yyin;
 extern int yyparse();
 extern std::stack<int> Indent;
 
-int main( int Argc, char **Argv )
+int main( int Argc, char *Argv[] )
 {
     Indent.push( 0 );
+    
+    FILE *Fp = nullptr;
+    
+    if( Argc == 2)
+    {   
+        FILE *Fp = fopen( Argv[1], "r" );
+
+        if( !Fp )
+        {
+            perror( Argv[1]);
+            return( 1 );
+        }
+        
+        yyin = Fp;
+    }
+
 
     yyparse();
-}
 
+    if( Fp != nullptr )
+        fclose( Fp );
+}
